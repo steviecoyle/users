aa0ff49f1392c63fe80714f14f72a385ca403d54
Stephen Coyle <steviecoyle@hotmail.com>
2024-12-04 23:09:29 +0000

    refactor GenericErrorResponse & GlobalExceptionHandler to use string date and date conversion class

--- src/main/java/com/scoyle/users/exception/GlobalExceptionHandler.java
+++ src/main/java/com/scoyle/users/exception/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package com.scoyle.users.exception;
 
-import org.joda.time.DateTime;
+import com.scoyle.users.utils.DateConversion;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -14,8 +13,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<GenericErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
         GenericErrorResponse errorDetails =
-                new GenericErrorResponse(new DateTime(), ex.getMessage());
+                new GenericErrorResponse(DateConversion.formatDateToAppFormat(), ex.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
-
 }
